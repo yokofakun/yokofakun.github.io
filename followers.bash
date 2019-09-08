6530a8f180c42a6ed0ccffe05755ad299849de9e
Pierre Lindenbaum <plindenbaum@yahoo.fr>
2019-09-08 23:15:28 +0200

    cont

---
+++ followers.bash
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+set -o pipefail
+
+touch followers.txt followers.last.txt
+
+
+wget -q -O - "https://mysocialmate.com/u/yokofakun/followers" |\
+xsltproc --html extract.xsl - 2>/dev/null | sort | uniq > tmp.followers.txt
+
+test -s tmp.followers.txt
+
+comm -3 followers.last.txt tmp.followers.txt
+mv tmp.followers.txt followers.last.txt
+
+cat followers.last.txt followers.txt | sort | uniq > tmp.followers.txt
+
+mv tmp.followers.txt followers.txt
+
+wc -l followers.txt
